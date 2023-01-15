f7dbc8b50648b8a7b8b593e87cb975afe79997e6
sebastian-riggio <sebastian.riggio@gmail.com>
2023-01-15 16:16:15 +0100

    pre entrega3

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -5,6 +5,8 @@ class Curso(models.Model):
 
     nombre=models.CharField(max_length=40)
     camada = models.IntegerField()
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Camada {self.camada}"
 
 
 class Estudiante(models.Model):
@@ -17,6 +19,8 @@ class Profesor(models.Model):
     apellido= models.CharField(max_length=30)
     email= models.EmailField()
     profesion= models.CharField(max_length=30)
+    def __str__(self):
+            return f"Nombre: {self.nombre} - Apellido {self.camada} - Email {self.camada} - Profesion {self.camada} "
 
 class Entregable(models.Model):
     nombre= models.CharField(max_length=30)
